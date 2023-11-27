244be9d5ccce5694167804770c92ee5ed981a1f6
shanuka2000 <shanuka.inbox@gmail.com>
2023-11-27 22:28:41 +0530

    navigation related links added

---
+++ constant/index.ts
@@ -0,0 +1,6 @@
+export const navLinks = [
+  { name: "Services", href: "/services" },
+  { name: "Products", href: "/products" },
+  { name: "About Us", href: "/about Us" },
+  { name: "Contact Us", href: "/Contact" },
+];
