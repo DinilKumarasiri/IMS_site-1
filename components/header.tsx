bf9aefc5100b4a10633f007b81bddff5a7304237
shanuka2000 <shanuka.inbox@gmail.com>
2023-11-27 23:53:05 +0530

    Added react icons and added small screen side menu

--- components/header.tsx
+++ components/header.tsx
@@ -2,11 +2,28 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { navLinks } from "@/constant";
+import { LuMenu, LuX } from "react-icons/lu";
 import { motion } from "framer-motion";
 
 const Header = () => {
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const handleResize = () => {
+    if (window.innerWidth >= 768) {
+      setMenuVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <header className="z-[999] relative text-black">
       <nav className="flex fixed items-center py-4 px-6 lg:px-[10rem] w-full shadow-xl select-none bg-white">
@@ -41,10 +58,37 @@ const Header = () => {
               </motion.li>
             ))}
           </ul>
+          <button
+            className="text-2xl p-2 rounded-full md:hidden hover:bg-gray-500/20 text-[#F39C12]"
+            aria-label="Aria Menu"
+            onClick={() => {
+              setMenuVisible(!menuVisible);
+            }}
+          >
+            {menuVisible ? <LuX /> : <LuMenu />}
+          </button>
         </div>
       </nav>
+      {menuVisible && <SideMenu />}
     </header>
   );
 };
 
 export default Header;
+
+export const SideMenu = () => {
+  return (
+    <ul className="fixed right-2 top-[5rem] p-2 flex flex-col gap-4 w-[10rem] items-start bg-white shadow-xl rounded-md">
+      {navLinks.map((item, index) => (
+        <li
+          key={index}
+          className="w-full hover:bg-gray-600/10  py-1 rounded-md"
+        >
+          <Link href={item.href} className="ml-[5px]">
+            {item.name}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
