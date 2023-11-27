9f8b1176114cf3359cc25683f7d71f2ca8e88567
shanuka2000 <shanuka.inbox@gmail.com>
2023-11-28 03:03:31 +0530

    Minor update to nav link location

--- constant/index.ts
+++ constant/index.ts
@@ -1,6 +1,6 @@
 export const navLinks = [
+  { name: "About Us", href: "/about Us" },
   { name: "Services", href: "/services" },
   { name: "Products", href: "/products" },
-  { name: "About Us", href: "/about Us" },
   { name: "Contact Us", href: "/Contact" },
 ];
