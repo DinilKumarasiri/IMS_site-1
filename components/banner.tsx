c0d183748b3c4a22a615ef3071a8a1c3a7993ad0
shanuka2000 <shanuka.inbox@gmail.com>
2023-11-29 16:33:40 +0530

    Updated banner section alignments

--- components/banner.tsx
+++ components/banner.tsx
@@ -6,14 +6,15 @@ import Link from "next/link";
 const Bannner = () => {
   return (
     <section className="relative h-screen w-full overflow-hidden items-start justify-start ">
-      <div className="z-1 absolute w-screen h-screen bg-[url('/background.jpg')] bg-cover grayscale brightness-75 opacity-75"></div>
-      <div className="z-10 px-10 flex flex-col gap-5">
+      <div className="z-1 absolute w-screen h-screen bg-[url('/background.jpg')] bg-cover bg-center grayscale brightness-50 opacity-85 md:brightness-75 md:opacity-75"></div>
+      <div className="z-10 px-10 py-16 md:py-0 flex flex-col gap-5">
         <motion.h1
           initial={{ opacity: 0, y: 100 }}
           animate={{ opacity: 1, y: 0 }}
-          className="z-10 text-3xl md:text-5xl lg:text-7xl font-medium mt-[3rem] md:mt-[7rem] text-white xl:mt-30"
+          className="z-10 text-4xl md:text-5xl lg:text-7xl font-medium mt-[3rem] md:mt-[7rem] text-white xl:mt-30"
         >
-          Innovative Solutions, Seamless Integration
+          Innovative Solutions, Seamless <br className="hidden lg:block" />{" "}
+          Integration
         </motion.h1>
         <motion.h3
           initial={{ opacity: 0, y: 100 }}
@@ -21,7 +22,7 @@ const Bannner = () => {
           transition={{
             delay: 0.1,
           }}
-          className="z-10 text-left text-sm text-md md:text-xl text-white"
+          className="z-10 text-left text-base text-md md:text-xl text-white"
         >
           Driving Excellence in Test Measurement and Industry 4.0 Applications
         </motion.h3>
@@ -31,7 +32,7 @@ const Bannner = () => {
           transition={{
             delay: 0.2,
           }}
-          className="z-10 flex flex-col gap-2 md:flex-row"
+          className="z-10 w-[10rem] md:w-auto flex flex-col gap-2 md:flex-row"
         >
           <Link
             href="/#"
