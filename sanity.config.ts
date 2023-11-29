965036f1b3c732652f6214690ad5a242f71837fb
shanuka2000 <shanuka.inbox@gmail.com>
2023-11-29 18:07:58 +0530

    fixed sanity config issue

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,8 +3,8 @@ import { deskTool } from "sanity/desk";
 import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./schemas";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET as string;
 
 export default defineConfig({
   basePath: "/studio",
