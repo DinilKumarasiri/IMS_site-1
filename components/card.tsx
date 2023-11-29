a30a62cedc016df06129ee0825b996e355d1b1ab
shanuka2000 <shanuka.inbox@gmail.com>
2023-11-30 04:01:14 +0530

    Updated card design for product and service

--- components/card.tsx
+++ components/card.tsx
@@ -11,19 +11,21 @@ type CardProps = {
 
 const Card = ({ title, description, image }: CardProps) => {
   return (
-    <div className=" w-[20rem] h-[30rem] shadow-2xl rounded-xl flex flex-col gap-5 items-center justify-between pb-5 pt-2 border-2 border-black">
-      <div className="w-full h-[60%] px-1">
+    <div className="w-[20rem] h-[30rem] custom-shadow rounded-2xl flex flex-col gap-5 items-center justify-between py-5 bg-[#355fc18f]">
+      <div className="w-[90%] h-[15rem] px-1 bg-white rounded-tl-2xl rounded-br-2xl mx-auto">
         <Image
           src={urlForImage(image).url()}
           alt={title}
-          width={400}
-          height={400}
-          className="w-full h-full object-contain rounded-t-xl"
+          width={300}
+          height={300}
+          className="w-full h-[15rem] object-contain rounded-t-xl"
         />
       </div>
-      <div className=" flex-1 mt-5 px-4 flex flex-col items-start justify-start gap-4">
-        <h4 className="font-semibold">{title}</h4>
-        <p className="line-clamp-5 text-sm text-gray-500 text-justify li">
+      <h4 className="font-semibold bg-[#f39d1290] w-full px-4 py-3 text-white text-center">
+        {title}
+      </h4>
+      <div className=" flex-1 mt-5 px-4 flex flex-col items-start justify-start gap-4 bg-transparent">
+        <p className="line-clamp-5 text-xs font-medium text-[#1f1f1f] text-justify">
           {description}
         </p>
       </div>
