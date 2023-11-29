019a73cb31a35c2515cf3f3bda0ebac88d93fbee
shanuka2000 <shanuka.inbox@gmail.com>
2023-11-29 16:24:50 +0530

    Image links array for partnet section

--- constant/index.ts
+++ constant/index.ts
@@ -4,3 +4,11 @@ export const navLinks = [
   { name: "Products", href: "/products" },
   { name: "Contact Us", href: "/Contact" },
 ];
+
+export const partnerImages = [
+  { alt: "ASC Sensors", imgUrl: "/cmp01.png" },
+  { alt: "Gantner", imgUrl: "/cmp02.png" },
+  { alt: "Hansford Sensors", imgUrl: "/cmp03.png" },
+  { alt: "Wilcoxen Sensors", imgUrl: "/cmp04.png" },
+  { alt: "KEMO", imgUrl: "/cmp05.png" },
+];
