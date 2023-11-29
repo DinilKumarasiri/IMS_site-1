b39d6df039c7cdc6180b40fb8ba52ea173bd5fe5
shanuka2000 <shanuka.inbox@gmail.com>
2023-11-30 04:00:38 +0530

    Updated heading font

--- components/about.tsx
+++ components/about.tsx
@@ -1,3 +1,4 @@
+import { fjalla } from "@/app/font";
 import React from "react";
 
 const About = () => {
@@ -6,7 +7,9 @@ const About = () => {
       className="py-10 px-10 md:px-40 flex flex-col items-center justify-center gap-10 bg-[#355fc18f] text-white scroll-mt-20"
       id="about"
     >
-      <h2 className="text-3xl md:text-5xl font-semibold">About Us</h2>
+      <h2 className={`${fjalla.className} text-3xl md:text-5xl font-semibold`}>
+        About Us
+      </h2>
       <p className="text-center">
         We are a premier high-tech solutions and service provider established in
         2014 and we have since expanded with branches and affiliate offices in
