e81b4a47f11669450a075ba758f4e9b1e1ee9d11
shanuka2000 <shanuka.inbox@gmail.com>
2023-12-01 03:06:02 +0530

    completed animation

--- components/animation.tsx
+++ components/animation.tsx
@@ -5,7 +5,10 @@ const Animation = () => {
   return (
     <div className="hidden md:block absolute right-0 w-[80%] h-[100%]">
       <div className="relative bg-blue w-full h-full">
-        <div className="z-10 absolute bottom-[20%] left-3 h-fit w-fit flex flex-col items-center justify-center">
+        <div
+          id="id01"
+          className="z-10 absolute bottom-[20%] left-3 h-fit w-fit flex flex-col items-center justify-center"
+        >
           <Image
             src="/tape-measure.png"
             alt="Measuring"
@@ -88,13 +91,11 @@ const Animation = () => {
           <Image
             src="/phone-vibration.png"
             alt="Measuring"
-            width={70}
-            height={70}
+            width={50}
+            height={50}
             className="w-auto h-auto bg-contain px-2 py-2 bg-gray-100 !object-contain rounded-full"
           />
-          <span className="font-semibold text-white text-lg">
-            Smart in Motion
-          </span>
+          <span className="font-semibold text-white">Smart in Motion</span>
         </div>
 
         <div className="z-10 absolute top-[40%] right-[35%] h-fit w-fit flex flex-col items-center justify-center">
@@ -116,12 +117,12 @@ const Animation = () => {
             height={40}
             className="w-auto h-auto bg-contain px-2 py-2 bg-gray-100 !object-contain rounded-full"
           />
-          <span className="font-semibold text-white text-sm">Testing</span>
+          <span className="font-semibold text-white text-sm">Monitoring</span>
         </div>
 
         <div className="z-10 absolute bottom-[25%] left-[40%] h-fit w-fit flex flex-col items-center justify-center">
           <Image
-            src="/check.png"
+            src="/testing.png"
             alt="Measuring"
             width={50}
             height={50}
@@ -129,18 +130,21 @@ const Animation = () => {
           />
           <span className="font-semibold text-white">Testing</span>
         </div>
-
-        <div className="-z-1 absolute bottom-[20%] left-[7%] rounded-md bg-gradient-to-r from-orange-500 to-black/10 w-[14%] h-[5px] rotate-[30deg] dashed-line"></div>
-        <div className="-z-1 absolute bottom-[45%] left-[27%] rounded-md bg-gradient-to-r from-red-500 to-black/10 w-[14%] h-[5px] !rotate-[20deg] animateScale"></div>
-        <div className="-z-1 absolute bottom-[22%] left-[45%] rounded-md bg-gradient-to-r from-blue-500 to-black/10 w-[14%] h-[5px] rotate-[37deg] dashed-line"></div>
-        <div className="-z-1 absolute bottom-[45%] left-[45%] rounded-md bg-gradient-to-r from-green-500 to-black/10 w-[14%] h-[5px] -rotate-[35deg] dashed-line"></div>
-        <div className="-z-1 absolute top-[35%] right-[20%] rounded-md bg-gradient-to-r from-green-500 to-black/10 w-[16%] h-[5px] -rotate-[30deg] dashed-line"></div>
-        <div className="-z-1 absolute bottom-[40%] left-[7%] rounded-md bg-gradient-to-r from-orange-500 to-black/10 w-[14%] h-[5px] rotate-[150deg] dashed-line"></div>
-        <div className="-z-1 absolute bottom-[30%] left-[17%] rounded-md bg-gradient-to-r from-orange-500 to-black/10 w-[14%] h-[5px] rotate-[90deg] dashed-line"></div>
-        <div className="-z-1 absolute bottom-[30%] left-[17%] rounded-md bg-gradient-to-r from-orange-500 to-black/10 w-[14%] h-[5px] rotate-[270deg] dashed-line"></div>
-        <div className="-z-1 absolute bottom-[31%] left-[9%] rounded-md bg-gradient-to-r from-orange-500 to-black/10 w-[30%] h-[5px] -rotate-[4deg] dashed-line"></div>
-        <div className="-z-1 absolute bottom-[10%] left-[27.5%] rounded-md bg-gradient-to-r from-orange-500 to-black/10 w-[27%] h-[5px] rotate-[deg] dashed-line"></div>
-        <div className="-z-1 absolute bottom-[10%] left-[63%] rounded-md bg-gradient-to-r from-orange-500 to-black/10 w-[20%] h-[5px] rotate-[deg] dashed-line"></div>
+        <div className="-z-1 absolute bottom-[45%] left-[20%] rounded-md bg-gradient-to-r from-orange-500 to-black/10 w-[14%] h-[5px] plane-anim-1"></div>
+        <div className="-z-1 absolute bottom-[42%] left-[23.5%] rounded-md bg-gradient-to-r from-orange-500 to-black/10 w-[14%] h-[5px] plane-anim-2"></div>
+        <div className="-z-1 absolute bottom-[45%] left-[27%] rounded-md bg-gradient-to-r from-orange-500 to-black/10 w-[14%] h-[5px] plane-anim-3"></div>
+        <div className="-z-1 absolute top-[48%] left-[28%] rounded-md bg-gradient-to-r from-orange-500 to-black/10 w-[55%] h-[5px] plane-anim-4"></div>
+        <div className="-z-1 absolute bottom-[31%] left-[9%] rounded-md bg-gradient-to-r from-orange-500 to-black/10 w-[30%] h-[5px] measuring-anim-1"></div>
+        <div className="-z-1 absolute bottom-[25%] left-[8%] rounded-md bg-gradient-to-r from-orange-500 to-black/10 w-[14%] h-[5px]  measuring-anim-2"></div>
+        <div className="-z-1 absolute bottom-[10%] left-[27.5%] rounded-md bg-gradient-to-r from-orange-500 to-black/10 w-[27%] h-[5px] ship-anim-1"></div>
+        <div className="-z-1 absolute bottom-[10%] left-[63%] rounded-md bg-gradient-to-r from-orange-500 to-black/10 w-[20%] h-[5px] train-anim-1"></div>
+        <div className="-z-1 absolute bottom-[37%] left-[46.5%] rounded-md bg-gradient-to-r from-orange-500 to-black/10 w-[14%] h-[5px] testing-anim-1"></div>
+        <div className="-z-1 absolute bottom-[29%] left-[46%] rounded-md bg-gradient-to-r from-orange-500 to-black/10 w-[14%] h-[5px] rotate-[37deg] testing-anim-2"></div>
+        <div className="-z-1 absolute top-[40%] right-[16%] rounded-md bg-gradient-to-r from-orange-500 to-black/10 w-[18%] h-[5px] testing-analyzing-1"></div>
+        <div className="-z-1 absolute top-[49%] right-[10%] rounded-md bg-gradient-to-r from-orange-500 to-black/10 w-[25%] h-[5px] testing-analyzing-2"></div>
+        <div className="-z-1 absolute top-[51%] right-[11%] rounded-md bg-gradient-to-r from-orange-500 to-black/10 w-[25%] h-[5px] testing-analyzing-3"></div>
+        <div className="-z-1 absolute bottom-[20%] right-[1%] rounded-md bg-gradient-to-r from-orange-500 to-black/10 w-[10%] h-[5px] testing-car-1"></div>
+        <div className="-z-1 absolute bottom-[49%] -right-[7%] rounded-md bg-gradient-to-r from-orange-500 to-black/10 w-[14%] h-[5px] testing-monitoring-1"></div>
       </div>
     </div>
   );
