2ec6e9adbf3c253a9ba33274dace1d0e6b742482
shanuka2000 <shanuka.inbox@gmail.com>
2023-12-04 01:03:12 +0530

    Updated products and services schema

---
+++ sanity/schemas/services.ts
@@ -0,0 +1,53 @@
+import { defineField, defineType } from "sanity";
+
+export default defineType({
+  name: "services",
+  type: "document",
+  title: "services",
+  fields: [
+    defineField({
+      name: "title",
+      title: "Title",
+      type: "string",
+      validation: (Rule) =>
+        Rule.max(40).error("Shorter titles are usually better"),
+    }),
+    defineField({
+      name: "summary",
+      title: "Summary",
+      type: "string",
+    }),
+    defineField({
+      title: "Slug",
+      name: "slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 200,
+        slugify: (input) =>
+          input.toLowerCase().replace(/\s+/g, "-").slice(0, 200),
+      },
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "url",
+      title: "URL",
+      type: "url",
+      validation: (Rule) =>
+        Rule.uri({
+          scheme: ["http", "https", "mailto", "tel"],
+        }),
+    }),
+    defineField({
+      name: "image",
+      title: "Image",
+      type: "image",
+    }),
+    defineField({
+      name: "content",
+      title: "Content",
+      type: "array",
+      of: [{ type: "block" }],
+    }),
+  ],
+});
