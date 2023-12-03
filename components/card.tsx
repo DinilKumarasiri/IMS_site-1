a8e0b43c37c73b539d92c26afe0663928b97f73c
shanuka2000 <shanuka.inbox@gmail.com>
2023-12-04 01:04:40 +0530

    Updated data displayed in card

--- components/card.tsx
+++ components/card.tsx
@@ -1,20 +1,13 @@
-import { urlForImage } from "@/lib/sanity.client";
-import { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import { List } from "@/typings";
 import Image from "next/image";
 import React from "react";
 
-type CardProps = {
-  title: string;
-  description: string;
-  image: SanityImageSource;
-};
-
-const Card = ({ title, description, image }: CardProps) => {
+const Card = ({ title, summary, image }: List) => {
   return (
-    <div className="w-[20rem] h-[30rem] custom-shadow rounded-2xl flex flex-col gap-5 items-center justify-between py-5 bg-[#355fc18f]">
+    <div className="w-[20rem] h-[30rem] custom-shadow rounded-2xl flex flex-col gap-5 items-center justify-between py-5 bg-[#355fc18f] hover:scale-105 transition">
       <div className="w-[90%] h-[15rem] px-1 bg-white rounded-tl-2xl rounded-br-2xl mx-auto">
         <Image
-          src={urlForImage(image).url()}
+          src={image}
           alt={title}
           width={300}
           height={300}
@@ -26,7 +19,7 @@ const Card = ({ title, description, image }: CardProps) => {
       </h4>
       <div className=" flex-1 mt-5 px-4 flex flex-col items-start justify-start gap-4 bg-transparent">
         <p className="line-clamp-5 text-xs font-medium text-[#1f1f1f] text-justify dark:text-white">
-          {description}
+          {summary}
         </p>
       </div>
     </div>
