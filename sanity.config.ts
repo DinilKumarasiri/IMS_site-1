9aab8cf6ef5a0efed5b2d6ff840585a6004ef3aa
2019479d <62051752+2019479d@users.noreply.github.com>
2024-01-25 09:28:41 +0530

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -5,7 +5,7 @@ import { schemaTypes } from "./sanity/schemas";
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET as string;
-
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION as string;
 export default defineConfig({
   basePath: "/studio",
   name: "ISM_Content_Admin",
@@ -13,6 +13,7 @@ export default defineConfig({
 
   projectId,
   dataset,
+  apiVersion,
 
   plugins: [deskTool(), visionTool()],
 
