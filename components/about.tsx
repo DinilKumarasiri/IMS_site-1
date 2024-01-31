b1ea3aa369b3b992823c872f99d9821cc305bfa6
sajeewaml9930 <sajeewaml@gmail.com>
2024-01-31 16:13:27 +0530

    Test Update10

--- components/about.tsx
+++ components/about.tsx
@@ -17,7 +17,7 @@ const About = () => {
         world-class and cutting-edge technology products and solutions across
         test measurement markets.
       </p>
-      <div className="flex flex-col md:flex-row items-center content-start justify-evenly pb-10 pt-10 md:px-[15rem] gap-[2.5rem] md:gap-[5rem]">
+      <div className="text-center flex flex-col md:flex-row items-center content-start justify-evenly pb-10 pt-10 md:px-[15rem] gap-[2.5rem] md:gap-[5rem]">
         <div className="flex flex-col items-center md:items-start gap-1">
           <p className="font-semibold">Our Expertise</p>
         <p>Over decades of experience in test measurement industry and innovative solutions for delivering ioT & Industry 4.0 applications,  structural monitoring,  Noise and Vibration monitoring applications across the SEA region.</p>
