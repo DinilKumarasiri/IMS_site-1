ed77974244d0b13934bbac211d1e7ef417cf17aa
sajeewaml9930 <sajeewaml@gmail.com>
2024-01-31 16:40:46 +0530

    Test Update10

--- components/about.tsx
+++ components/about.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const About = () => {
   return (
     <section
-      className="py-10 px-10 md:px-40 flex flex-col items-center justify-center gap-10 bg-[#355fc18f] text-white scroll-mt-20"
+      className="py-10 px-10 md:px-40 flex flex-col items-center justify-center gap-10 bg-[#355fc18f] text-black dark:text-white scroll-mt-20"
       id="about"
     >
       <h2 className={`${fjalla.className} text-3xl md:text-5xl font-semibold`}>
@@ -19,11 +19,11 @@ const About = () => {
       </p>
       <div className="text-center flex flex-col md:flex-row items-center content-start justify-evenly pb-10 pt-10 md:px-[15rem] gap-[2.5rem] md:gap-[5rem]">
         <div className="flex flex-col items-center md:items-start gap-1">
-          <p className="font-semibold text-center text-2xl">Our Expertise</p>
+          <p className="text-center font-semibold text-2xl">Our Expertise</p>
         <p>Over decades of experience in test measurement industry and innovative solutions for delivering ioT & Industry 4.0 applications,  structural monitoring,  Noise and Vibration monitoring applications across the SEA region.</p>
         </div>
         <div className="flex flex-col items-center md:items-start gap-1">
-        <p className="font-semibold text-center text-2xl">Services</p>
+        <p className="text-center font-semibold text-2xl">Services</p>
           <p>Mechanical Drawings, Short term and Long term hardware rental for Vibration monitoring applications, Customized Data Loggers and Applications.</p>
         </div>
       </div>
