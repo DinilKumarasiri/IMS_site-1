fbf720078a9c7373cbf9e1cc2acd96aa6e7b42ec
sajeewaml9930 <sajeewaml@gmail.com>
2024-01-31 16:16:45 +0530

    Test Update10

--- components/about.tsx
+++ components/about.tsx
@@ -19,11 +19,11 @@ const About = () => {
       </p>
       <div className="text-center flex flex-col md:flex-row items-center content-start justify-evenly pb-10 pt-10 md:px-[15rem] gap-[2.5rem] md:gap-[5rem]">
         <div className="flex flex-col items-center md:items-start gap-1">
-          <p className="font-semibold">Our Expertise</p>
+          <p className="font-semibold text-center text-2xl">Our Expertise</p>
         <p>Over decades of experience in test measurement industry and innovative solutions for delivering ioT & Industry 4.0 applications,  structural monitoring,  Noise and Vibration monitoring applications across the SEA region.</p>
         </div>
         <div className="flex flex-col items-center md:items-start gap-1">
-        <p className="font-semibold">Services</p>
+        <p className="font-semibold text-center text-2xl">Services</p>
           <p>Mechanical Drawings, Short term and Long term hardware rental for Vibration monitoring applications, Customized Data Loggers and Applications.</p>
         </div>
       </div>
