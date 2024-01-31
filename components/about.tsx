ff180d59284b3cd869a431d4deb98fe068abb69a
sajeewaml9930 <sajeewaml@gmail.com>
2024-01-31 18:54:59 +0530

    Test Update11

--- components/about.tsx
+++ components/about.tsx
@@ -18,11 +18,12 @@ const About = () => {
         test measurement markets.
       </p>
       <div className="text-center flex flex-col md:flex-row content-start justify-stretch pb-10 pt-10 md:px-[15rem] gap-[2.5rem] md:gap-[5rem]">
-
-          <p><span className="text-center font-semibold text-2xl">Our Expertise<br/><br/></span>Over decades of experience in test measurement industry and innovative solutions for delivering ioT & Industry 4.0 applications,  structural monitoring,  Noise and Vibration monitoring applications across the SEA region.</p>
-
-          <p><span className="text-center font-semibold text-2xl">Services<br/><br/></span>Mechanical Drawings, Short term and Long term hardware rental for Vibration monitoring applications, Customized Data Loggers and Applications.</p>
-
+        <div className="flex flex-col md:items-start gap-1">
+          <p><span className="text-center font-semibold text-2xl">Our Expertise<br/></span>Over decades of experience in test measurement industry and innovative solutions for delivering ioT & Industry 4.0 applications,  structural monitoring,  Noise and Vibration monitoring applications across the SEA region.</p>
+        </div>
+        <div className="flex flex-col md:items-start gap-1">
+          <p><span className="text-center font-semibold text-2xl">Services<br/></span>Mechanical Drawings, Short term and Long term hardware rental for Vibration monitoring applications, Customized Data Loggers and Applications.</p>
+        </div>
       </div>
       </section>
   );
